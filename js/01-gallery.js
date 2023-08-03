5d5c56cffdf5b48f1e0b346e72cc7a94dde4a47c
AVasilaki <poseydon463@gmail.com>
2023-08-03 12:14:17 +0300

    dz7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+// import * as basicLightbox from "basiclightbox";
+const container = document.querySelector(".gallery");
 
-console.log(galleryItems);
+function createMurcup(arr) {
+  console.log(arr);
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+      <a class="gallery__link" href="large-image.jpg">
+        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+      </a>
+    </li>`
+    )
+    .join("");
+}
+container.insertAdjacentHTML("beforeend", createMurcup(galleryItems));
+container.addEventListener("click", handlerClick);
+function handlerClick(evt) {
+  evt.preventDefault();
+  const selectedImg = evt.target.dataset.source;
+
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src= ${selectedImg} width="800" height="600">
+`);
+
+  instance.show();
+}

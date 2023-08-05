deaa01ba08784f4be4fc8d37f51a1eb3a96936e5
AVasilaki <poseydon463@gmail.com>
2023-08-05 13:24:45 +0300

    dz7 task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const container = document.querySelector(".gallery");
 
-console.log(galleryItems);
+function createMurcup(arr) {
+  console.log(arr);
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}"  alt="${description}" />
+      </a>
+    </li>`
+    )
+    .join("");
+}
+container.insertAdjacentHTML("beforeend", createMurcup(galleryItems));
+// console.log(galleryItems);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
